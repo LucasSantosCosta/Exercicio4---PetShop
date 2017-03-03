2bf3d7422a6d68e0093cb1d5effa33d3221b0069
Lucas Santos <santos.lucas00@hotmail.com>
2017-03-03 14:36:01 -0300

    first commit

---
+++ app/src/main/java/petshop/rm78785/desafio4/fiap/com/br/petshop/MainActivity.java
@@ -0,0 +1,13 @@
+package petshop.rm78785.desafio4.fiap.com.br.petshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
